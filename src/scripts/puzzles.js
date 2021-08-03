8805bd3060f3e6db0c6026113e3712b2f0674d37
webbdevv <kyle.xu4@gmail.com>
2021-08-03 09:01:01 -0700

    index changes

--- src/scripts/puzzles.js
+++ src/scripts/puzzles.js
@@ -17,24 +17,27 @@ export default [
   {
     id: 3,
     header: `Enti-cide?`,
-    hint: `Recursion`
+    hint: `Make sure to call destroyAll recursively`
   },
   {
     id: 4,
     header: 'Soles for Souls',
-    hint: `Check the entity for movement directions. console.log will help`
+    hint: `Add the move to the current position of an entity`
   },
   {
     id: 5,
     header: "The Speed of Light",
-    hint: ``
+    hint: `Quite the long one, make sure to move carefully. Use the difference between the start and end position to move the entity.
+    In case of emergency, hit the Get Answer button`
   },
   {
     id: 6,
-    header: "Light's First Steps!!!"
+    header: "Light's First Steps!!!",
+    hint: `Move using the function you wrote`
   },
   {
     id: 7,
-    header: "Maria, Rose, and Sheena"
+    header: "Maria, Rose, and Sheena",
+    hint: `Use the grid to move walls around.`
   }
 ]
